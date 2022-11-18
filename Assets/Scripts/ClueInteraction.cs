a17d7fe1ad03ae9d9a14bb6cae102b5a48d92442
drock2024 <drock2024@gmail.com>
2022-11-17 22:08:37 -0500

    Added clue interaction

---
+++ Assets/Scripts/ClueInteraction.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ClueInteraction : MonoBehaviour
+{
+    //Define text and interactors
+    public GameObject textBox;
+    public GameObject player;
+    public GameObject journal;
+
+    //Define variables
+    float distance = 0;
+    bool active = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f") && distance < 5) {
+            active = !active;
+        }
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance < 5) {
+            if (!active) {
+                textBox.GetComponent<TextMeshProUGUI>().text = "*Press f to inspect*";
+                journal.GetComponent<Image>().enabled = false;
+            } else if (active) {
+                textBox.GetComponent<TextMeshProUGUI>().text = "";
+                journal.GetComponent<Image>().enabled = true;
+            }
+        }
+    }
+}
